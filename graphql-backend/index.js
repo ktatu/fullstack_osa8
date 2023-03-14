3e1a6660749188a1be8e9a1884ca20cb40989cb1
ktatu <kaikkonentatu@gmail.com>
2023-03-14 03:01:29 +0200

    8.6

--- graphql-backend/index.js
+++ graphql-backend/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server")
 const { startStandaloneServer } = require("@apollo/server/standalone")
+const { v1: uuid } = require("uuid")
 
 let authors = [
     {
@@ -112,6 +113,15 @@ const typeDefs = `
         allBooks(author: String, genre: String): [Book!]!
         allAuthors: [Author!]!
     }
+
+    type Mutation {
+        addBook(
+            title: String!
+            author: String!
+            published: Int!
+            genres: [String!]!
+        ): Book
+    }
 `
 
 const resolvers = {
@@ -136,6 +146,18 @@ const resolvers = {
     },
     Author: {
         bookCount: (root) => books.filter((book) => book.author === root.name).length,
+    },
+    Mutation: {
+        addBook: (root, args) => {
+            const book = { ...args, id: uuid() }
+            books = books.concat(book)
+
+            if (!authors.includes(book.author)) {
+                authors = authors.concat({ name: book.author, born: null, id: uuid() })
+            }
+
+            return book
+        },
     },
 }
 
