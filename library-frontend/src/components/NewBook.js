a5262900df5181471f807e623da4be379ea35a1e
ktatu <kaikkonentatu@gmail.com>
2023-03-25 03:44:32 +0200

    8.10

--- library-frontend/src/components/NewBook.js
+++ library-frontend/src/components/NewBook.js
@@ -1,72 +1,82 @@
-import { useState } from 'react'
+import { useState } from "react"
+import { useMutation } from "@apollo/client"
+import { CREATE_BOOK, ALL_BOOKS, ALL_AUTHORS } from "../queries"
 
 const NewBook = (props) => {
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [published, setPublished] = useState('')
-  const [genre, setGenre] = useState('')
-  const [genres, setGenres] = useState([])
+    const [title, setTitle] = useState("")
+    const [author, setAuthor] = useState("")
+    const [published, setPublished] = useState("")
+    const [genre, setGenre] = useState("")
+    const [genres, setGenres] = useState([])
 
-  if (!props.show) {
-    return null
-  }
+    const [createBook] = useMutation(CREATE_BOOK)
 
-  const submit = async (event) => {
-    event.preventDefault()
+    if (!props.show) {
+        return null
+    }
 
-    console.log('add book...')
+    const submit = async (event) => {
+        event.preventDefault()
 
-    setTitle('')
-    setPublished('')
-    setAuthor('')
-    setGenres([])
-    setGenre('')
-  }
+        createBook({
+            variables: { title, author, published: Number(published), genres },
+            refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }],
+        })
 
-  const addGenre = () => {
-    setGenres(genres.concat(genre))
-    setGenre('')
-  }
+        setTitle("")
+        setPublished("")
+        setAuthor("")
+        setGenres([])
+        setGenre("")
+    }
 
-  return (
-    <div>
-      <form onSubmit={submit}>
+    const addGenre = () => {
+        setGenres(genres.concat(genre))
+        setGenre("")
+    }
+
+    return (
         <div>
-          title
-          <input
-            value={title}
-            onChange={({ target }) => setTitle(target.value)}
-          />
+            <form onSubmit={submit}>
+                <div>
+                    title
+                    <input
+                        value={title}
+                        onChange={({ target }) => setTitle(target.value)}
+                    />
+                </div>
+                <div>
+                    author
+                    <input
+                        value={author}
+                        onChange={({ target }) => setAuthor(target.value)}
+                    />
+                </div>
+                <div>
+                    published
+                    <input
+                        type="number"
+                        value={published}
+                        onChange={({ target }) => setPublished(target.value)}
+                    />
+                </div>
+                <div>
+                    <input
+                        value={genre}
+                        onChange={({ target }) => setGenre(target.value)}
+                    />
+                    <button
+                        onClick={addGenre}
+                        type="button"
+                    >
+                        add genre
+                    </button>
+                </div>
+                <div>genres: {genres.join(" ")}</div>
+                <button type="submit">create book</button>
+            </form>
         </div>
-        <div>
-          author
-          <input
-            value={author}
-            onChange={({ target }) => setAuthor(target.value)}
-          />
-        </div>
-        <div>
-          published
-          <input
-            type="number"
-            value={published}
-            onChange={({ target }) => setPublished(target.value)}
-          />
-        </div>
-        <div>
-          <input
-            value={genre}
-            onChange={({ target }) => setGenre(target.value)}
-          />
-          <button onClick={addGenre} type="button">
-            add genre
-          </button>
-        </div>
-        <div>genres: {genres.join(' ')}</div>
-        <button type="submit">create book</button>
-      </form>
-    </div>
-  )
+    )
 }
 
 export default NewBook
--- library-frontend/src/queries.js
+++ library-frontend/src/queries.js
@@ -20,4 +20,15 @@ const ALL_BOOKS = gql`
     }
 `
 
-export { ALL_AUTHORS, ALL_BOOKS }
+const CREATE_BOOK = gql`
+    mutation createBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+        addBook(title: $title, author: $author, published: $published, genres: $genres) {
+            title
+            author
+            published
+            genres
+        }
+    }
+`
+
+export { ALL_AUTHORS, ALL_BOOKS, CREATE_BOOK }
