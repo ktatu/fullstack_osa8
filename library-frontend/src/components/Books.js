9733745e271b483ebfae79dc4ce7a05bc8dbc9c3
ktatu <kaikkonentatu@gmail.com>
2023-03-25 02:56:55 +0200

    8.9

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,32 +1,39 @@
+import { useQuery } from "@apollo/client"
+import { ALL_BOOKS } from "../queries"
+
 const Books = (props) => {
-  if (!props.show) {
-    return null
-  }
+    const result = useQuery(ALL_BOOKS)
 
-  const books = []
+    if (!props.show) {
+        return null
+    }
 
-  return (
-    <div>
-      <h2>books</h2>
+    if (result.loading) {
+        return <div>Loading...</div>
+    }
 
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>author</th>
-            <th>published</th>
-          </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
+    return (
+        <div>
+            <h2>books</h2>
+
+            <table>
+                <tbody>
+                    <tr>
+                        <th></th>
+                        <th>author</th>
+                        <th>published</th>
+                    </tr>
+                    {result.data.allBooks.map((a) => (
+                        <tr key={a.title}>
+                            <td>{a.title}</td>
+                            <td>{a.author}</td>
+                            <td>{a.published}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default Books
